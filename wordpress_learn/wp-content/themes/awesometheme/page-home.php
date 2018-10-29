3e2e4c7b72e96c6311eac5e8de4219d7d9ef5a6d
unknown <nosracer_gt@sbcglobal.net>
2018-10-29 01:18:33 -0500

    Learned about limit post per page and navagating post pages

--- wordpress_learn/wp-content/themes/awesometheme/page-home.php
+++ wordpress_learn/wp-content/themes/awesometheme/page-home.php
@@ -2,48 +2,86 @@
 
 <div class="row">
 		
-        <?php 
-        
-            $args_cat = array(
-                'include' => '8,7,6'
-            );
-
-            $categories = get_categories($args_cat);
-            foreach($categories as $category):
-                
-                $args = array(
-                    'type' => 'post',
-                    'posts_per_page' => 1,
-                    'category__in' => $category->term_id,
-                    'category__not_in' => array(9)
-                );
-                
-                $lastBlog = new WP_Query($args);
-                
-                if( $lastBlog->have_posts() ):
-                    
-                    while( $lastBlog->have_posts() ): $lastBlog->the_post(); ?>
-    
-                        <div class="col-xs-12 col-sm-4">
-                        
-                            <?php get_template_part('content','featured'); ?>
-    
-                        </div>
-                        
-                    
-                    <?php endwhile;
-                    
-                endif;
-                
-                wp_reset_postdata();
-
-            endforeach;
+		<div class="col-xs-12">
 		
-		?>
+		<div id="awesome-carousel" class="carousel slide" data-ride="carousel">
 		
-	</div>
+		  <!-- Wrapper for slides -->
+		  <div class="carousel-inner" role="listbox">
+			  
+			<?php 
+			
+				$args_cat = array(
+					'include' => '1, 9, 8'
+				);
+				
+				$categories = get_categories($args_cat);
+				$count = 0;
+				$bullets = '';
+				foreach($categories as $category):
+					
+					$args = array( 
+						'type' => 'post',
+						'posts_per_page' => 1,
+						'category__in' => $category->term_id,
+						'category__not_in' => array( 10 ),
+					);
+					
+					$lastBlog = new WP_Query( $args ); 
+					
+					if( $lastBlog->have_posts() ):
+						
+						while( $lastBlog->have_posts() ): $lastBlog->the_post(); ?>
+							
+							<div class="item <?php if($count == 0): echo 'active'; endif; ?>">
+						      <?php the_post_thumbnail('full'); ?>
+						      <div class="carousel-caption">
+							      <?php the_title( sprintf('<h1 class="entry-title"><a href="%s">', esc_url( get_permalink() ) ),'</a></h1>' ); ?>
+	
+								  <small><?php the_category(' '); ?></small>
+						      </div>
+						    </div>
+						    
+						    <?php $bullets .= '<li data-target="#awesome-carousel" data-slide-to="'.$count.'" class="'; ?>
+						    <?php if($count == 0): $bullets .='active'; endif; ?>
+						    
+						    <?php  $bullets .= '"></li>'; ?>
+						
+						<?php endwhile;
+						
+					endif;
+					
+					wp_reset_postdata();
+				
+				$count++;
+					
+				endforeach;
+			
+			?>
+			
+			<!-- Indicators -->
+			  <ol class="carousel-indicators">
+			    <?php echo $bullets; ?>
+			  </ol>
+		    
+		  </div>
+		
+		  <!-- Controls -->
+		  <a class="left carousel-control" href="#awesome-carousel" role="button" data-slide="prev">
+		    <span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>
+		    <span class="sr-only">Previous</span>
+		  </a>
+		  <a class="right carousel-control" href="#awesome-carousel" role="button" data-slide="next">
+		    <span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>
+		    <span class="sr-only">Next</span>
+		  </a>
+		</div>
+		
+		</div>
+		
+</div>
 
-    <div class="row">
+<div class="row">
 	
 	<div class="col-xs-12 col-sm-8">
 
@@ -57,10 +95,10 @@
 			
 			<?php endwhile;
 			
-        endif;
-        
-/*		
+		endif;
+		
 		//PRINT OTHER 2 POSTS NOT THE FIRST ONE
+/*
 		$args = array(
 			'type' => 'post',
 			'posts_per_page' => 2,
@@ -80,14 +118,16 @@
 		endif;
 		
 		wp_reset_postdata();
-*/				
+*/
+				
 		?>
 		
-		<hr>
+		<!-- <hr> -->
 		
 		<?php
-/*			
+			
 		//PRINT ONLY TUTORIALS
+/*
 		$lastBlog = new WP_Query('type=post&posts_per_page=-1&category_name=news');
 			
 		if( $lastBlog->have_posts() ):
@@ -101,9 +141,10 @@
 		endif;
 		
 		wp_reset_postdata();
-*/				
+*/
+				
 		?>
-
+	
 	</div>
 	
 	<div class="col-xs-12 col-sm-4">
