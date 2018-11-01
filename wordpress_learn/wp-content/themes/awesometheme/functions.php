5a52eee0c485146c080a78cb83021a33eb0f6415
unknown <nosracer_gt@sbcglobal.net>
2018-11-01 01:37:44 -0500

    learned a bit on created custom taxonomy for portfolio custom post type

--- wordpress_learn/wp-content/themes/awesometheme/functions.php
+++ wordpress_learn/wp-content/themes/awesometheme/functions.php
@@ -1,132 +1,171 @@
 <?php
 /*
-	==========================================
-	 Include scripts
-	==========================================
-*/
-function awesome_script_enqueue() {
-	//css
-	wp_enqueue_style('bootstrap', get_template_directory_uri() . '/css/bootstrap.min.css', array(), '3.3.4', 'all');
-	wp_enqueue_style('customstyle', get_template_directory_uri() . '/css/awesome.css', array(), '1.0.0', 'all');
-	//js
-	wp_enqueue_script('jquery');
-	wp_enqueue_script('bootstrapjs', get_template_directory_uri() . '/js/bootstrap.min.js', array(), '3.3.4', true);
-	wp_enqueue_script('customjs', get_template_directory_uri() . '/js/awesome.js', array(), '1.0.0', true);
-	
+==========================================
+Include scripts
+==========================================
+ */
+function awesome_script_enqueue()
+{
+    //css
+    wp_enqueue_style('bootstrap', get_template_directory_uri() . '/css/bootstrap.min.css', array(), '3.3.4', 'all');
+    wp_enqueue_style('customstyle', get_template_directory_uri() . '/css/awesome.css', array(), '1.0.0', 'all');
+    //js
+    wp_enqueue_script('jquery');
+    wp_enqueue_script('bootstrapjs', get_template_directory_uri() . '/js/bootstrap.min.js', array(), '3.3.4', true);
+    wp_enqueue_script('customjs', get_template_directory_uri() . '/js/awesome.js', array(), '1.0.0', true);
+
 }
 
-add_action( 'wp_enqueue_scripts', 'awesome_script_enqueue');
+add_action('wp_enqueue_scripts', 'awesome_script_enqueue');
 
 /*
-	==========================================
-	 Activate menus
-	==========================================
-*/
-function awesome_theme_setup() {
-	
-	add_theme_support('menus');
-	
-	register_nav_menu('primary', 'Primary Header Navigation');
-	register_nav_menu('secondary', 'Footer Navigation');
-	
+==========================================
+Activate menus
+==========================================
+ */
+function awesome_theme_setup()
+{
+
+    add_theme_support('menus');
+
+    register_nav_menu('primary', 'Primary Header Navigation');
+    register_nav_menu('secondary', 'Footer Navigation');
+
 }
 
 add_action('init', 'awesome_theme_setup');
 
 /*
-	==========================================
-	 Theme support function
-	==========================================
-*/
+==========================================
+Theme support function
+==========================================
+ */
 add_theme_support('custom-background');
 add_theme_support('custom-header');
 add_theme_support('post-thumbnails');
-add_theme_support('post-formats',array('aside','image','video'));
+add_theme_support('post-formats', array('aside', 'image', 'video'));
 add_theme_support('html5', array('search-form'));
 
 /*
-	==========================================
-	 Sidebar function
-	==========================================
-*/
-function awesome_widget_setup() {
-	
-	register_sidebar(
-		array(	
-			'name'	=> 'Sidebar',
-			'id'	=> 'sidebar-1',
-			'class'	=> 'custom',
-			'description' => 'Standard Sidebar',
-			'before_widget' => '<aside id="%1$s" class="widget %2$s">',
-			'after_widget'  => '</aside>',
-			'before_title'  => '<h1 class="widget-title">',
-			'after_title'   => '</h1>',
-		)
-	);
-	
+==========================================
+Sidebar function
+==========================================
+ */
+function awesome_widget_setup()
+{
+
+    register_sidebar(
+        array(
+            'name' => 'Sidebar',
+            'id' => 'sidebar-1',
+            'class' => 'custom',
+            'description' => 'Standard Sidebar',
+            'before_widget' => '<aside id="%1$s" class="widget %2$s">',
+            'after_widget' => '</aside>',
+            'before_title' => '<h1 class="widget-title">',
+            'after_title' => '</h1>',
+        )
+    );
+
 }
-add_action('widgets_init','awesome_widget_setup');
+add_action('widgets_init', 'awesome_widget_setup');
 
 /*
-	==========================================
-	 Include Walker File
-	==========================================
-*/
+==========================================
+Include Walker File
+==========================================
+ */
 
 require get_template_directory() . '/inc/walker.php';
 
 /*
-	==========================================
-	 Head Function
-	==========================================
-*/
+==========================================
+Head Function
+==========================================
+ */
 
-function awesome_remove_version() {
-	return '';
+function awesome_remove_version()
+{
+    return '';
 }
 add_filter('the_generator', 'awesome_remove_version');
 
 /*
-	==========================================
-	 Custom Post Type
-	==========================================
-*/
+==========================================
+Custom Post Type
+==========================================
+ */
 
-function awesome_custom_post_type() {
-	$labels = array(
-		'name' => 'Portfolio',
-		'singular_name' => 'Portfolio',
-		'add_new' => 'Add Item',
-		'all_items' => 'All Items',
-		'add_new_item' => 'Add Item',
-		'edit_item' => 'Edit Item',
-		'new_item' => 'New Item',
-		'view_item' => 'View Item',
-		'search_item' => 'Search Portfolio',
-		'not_found' => 'No items found',
-		'not_found_in_trash' => 'No items found in trash',
-		'parent_item_colon' => 'parent Item'
-	);
-	$args = array(
-		'labels' => $labels,
-		'public' => true,
-		'has_archive' => true,
-		'publicly_queryable' => true,
-		'query_var' => true,
-		'rewrite' => true,
-		'capability_type' => 'post',
-		'hierarchical' => false,
-		'supports' => array(
-			'title',
-			'editor',
-			'excerpt',
-			'thumbnail',
-			'revisions'
-		),
-		'taxonomies' => array('category', 'post_tag'),
-		'menu_position' => 5,
-		'exclude_from_search' => false
-	);
-	register_post_type('portfolio', $args);
+function awesome_custom_post_type()
+{
+    $labels = array(
+        'name' => 'Portfolio',
+        'singular_name' => 'Portfolio',
+        'add_new' => 'Add Item',
+        'all_items' => 'All Items',
+        'add_new_item' => 'Add Item',
+        'edit_item' => 'Edit Item',
+        'new_item' => 'New Item',
+        'view_item' => 'View Item',
+        'search_item' => 'Search Portfolio',
+        'not_found' => 'No items found',
+        'not_found_in_trash' => 'No items found in trash',
+        'parent_item_colon' => 'parent Item',
+    );
+    $args = array(
+        'labels' => $labels,
+        'public' => true,
+        'has_archive' => true,
+        'publicly_queryable' => true,
+        'query_var' => true,
+        'rewrite' => true,
+        'capability_type' => 'post',
+        'hierarchical' => false,
+        'supports' => array(
+            'title',
+            'editor',
+            'excerpt',
+            'thumbnail',
+            'revisions',
+        ),
+        // 'taxonomies' => array('category', 'post_tag'),
+        'menu_position' => 5,
+        'exclude_from_search' => false,
+    );
+    register_post_type('portfolio', $args);
 }
 add_action('init', 'awesome_custom_post_type');
+
+function awesome_custom_taxonomies()
+{
+	//add new taxonomy hierarchical
+	$labels = array(
+		'name' => 'Types',
+		'singular_name' => 'Type',
+		'search_items' => 'Search Types',
+		'all_items' => 'All Types',
+		'parent_item' => 'Parent Type',
+		'parent_item_colon' => 'Parent Type:',
+		'edit_item' => 'Edit Type',
+		'update_item' => 'Update Type',
+		'add_new_item' => 'Add New Type',
+		'new_item_name' => 'New Type Name',
+		'menu_name' => 'Type'
+	);
+
+	$args = array(
+		'hierarchical' => true,
+		'labels' => $labels,
+		'show_ui' => true,
+		'show_admin_column' => true,
+		'query_var' => true,
+		'rewrite' => array('slug' => 'type')
+		// mysite.com/development
+		// mysite.com/type/development
+	);
+	register_taxonomy('type', array('portfolio'), $args);
+
+	//add new taxonomy NOT hierarchical
+}
+
+add_action('init', 'awesome_custom_taxonomies');
